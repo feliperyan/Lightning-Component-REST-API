c8b81a113b38e0aca83a0c1352156f35bf486352
Felipe Ryan <feliperyan@gmail.com>
2016-04-15 15:55:09 +1000

    Changed component to refresh profile component

--- DisplaySocketEventComponentController.js
+++ DisplaySocketEventComponentController.js
@@ -1,7 +1,14 @@
 ({
 	handleWebSocketEvent : function(component, event, helper) {
         console.log('App event received.');
-		var message = event.getParam("websocket_message");
+		var message = event.getParam("websocket_message");        
         component.set("v.latestMessage", message);
+        
+        // Check whether we just received a record-id as an event
+        var theRecord = helper.isRecordId(component, event);
+        if (theRecord !== ''){
+            console.log('setting');
+            component.find('profileComponent').set("v.recordId", theRecord);            
+        }
 	}
 })
