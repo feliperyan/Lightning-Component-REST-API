ffed8bf1e68f23adce6710ac1a7b6d1a12a9cce7
Felipe Ryan <feliperyan@gmail.com>
2016-07-15 15:36:52 +1000

    replaced getElementById by an aura attribute

--- FryanVFComponentHelper.js
+++ FryanVFComponentHelper.js
@@ -1,7 +1,7 @@
 ({
 	listMessage : function(component, event) {
-        b = document.getElementById(component.getGlobalId() + 'bucket');
-        b.innerHTML += '<p>'+event.data.response+'</p>';
-		
+        //b = document.getElementById(component.getGlobalId() + 'bucket');
+        //b.innerHTML += '<p>'+event.data.response+'</p>';
+		component.set("v.status", event.data.response);
 	}
 })
